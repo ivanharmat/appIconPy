1d3817b50a5b28cf082d25d5170bfda3a48b42d0
Ivan Harmat <ivanharmat@yahoo.com>
2017-08-10 21:44:07 -0400

    working first version

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Overall goals of this script:
 
  * open an image file
  * check if the image is a square and at least 228x228
+ * loop through icon dimensions
+ * resize image and write to disk
+ * add resized image to zip archive
+ * remove resized image
+ * show success message or error
+
--- appIconPy.py
+++ appIconPy.py
@@ -1,18 +1,33 @@
 from PIL import Image
 import sys
 import os
+import zipfile
 
-basewidth = 100
-if len(sys.argv) > 1 :
+
+dimensions = [180, 167, 152, 120, 87, 80, 76, 60, 58, 40, 29, 20]
+
+if len(sys.argv) > 1 : 
 	try :
 		img = Image.open(sys.argv[1])
-		if img.size[0] == img.size[1] :
-			if img.size[0] >= 228 :
-				filename = "resized-"+os.path.basename(sys.argv[1])
-				wpercent = (basewidth / float(img.size[0]))
-				hsize = int((float(img.size[1]) * float(wpercent)))
-				img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-				img.save(filename)
+		if img.size[0] == img.size[1] : # image is square - icon
+			if img.size[0] >= 228 : # image is large enough
+				zipFileSavedToFolder = os.path.dirname(os.path.realpath(__file__))
+				zipFileName = "appIcons.zip"
+				zf = zipfile.ZipFile(zipFileName, mode='w')
+
+				for dimension in range(0, len(dimensions)) :
+					filename = str(dimensions[dimension])+"x"+str(dimensions[dimension])+"-"+os.path.basename(sys.argv[1])
+					# resize image
+					img = img.resize((dimensions[dimension], dimensions[dimension]), Image.ANTIALIAS)
+					# save to disk
+					img.save(filename)
+					# add to zip archive
+					zf.write(filename)
+					# remove from disk
+					os.remove(filename)
+
+				print("Success - the zip file was saved to "+zipFileSavedToFolder+"/"+zipFileName)
+				zf.close()
 			else :
 				print("Image is too small - minimum 228x228 needed")
 		else :
